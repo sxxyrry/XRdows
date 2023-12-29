efd73821b745521e6f447b2d90da46ec6cc942b1
是星星与然然呀（XR） <XR>
2023-12-29 15:05:59 +0800

    XRdows BRTA 1.0 Version

--- XRdows_exe/XRdows/_internal/XRdows.py
+++
@@ -1,4 +0,0 @@
-import XRdows_main as XRdows
-
-if __name__ == '__main__':
-    XRdows.XRdows_T.XRdows_N_D()
--- _XRdows_/XRdows.py
+++ _XRdows_/XRdows.py
@@ -2,3 +2,4 @@ import XRdows_main as XRdows
 
 if __name__ == '__main__':
     XRdows.XRdows_T.XRdows_N_D()
+
